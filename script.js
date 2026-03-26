a28de5c520038bcefbf44d9ce7b2fafca394811b
HarshaSatpute045 <satputeharsha2@gmail.com>
2026-03-26 12:06:29 +0530

    My first commit

---
+++ script.js
@@ -0,0 +1,101 @@
+const BASE_URL = "https://open.er-api.com/v6/latest";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } 
+    else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  // ✅ Correct API URL
+  const URL = `${BASE_URL}/${fromCurr.value}`;
+
+  let response = await fetch(URL);
+  let data = await response.json();
+
+  // ✅ Correct way to get rate
+  let rate = data.rates[toCurr.value];
+
+  let finalAmount = amtVal * rate;
+
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCurr.value}`;
+};
+
+const updateFlag = (element) => {
+
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+
+  let img = element.parentElement.querySelector("img");
+
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
+
+
+
+const swapBtn = document.querySelector(".fa-arrow-right-arrow-left");
+
+swapBtn.addEventListener("click", () => {
+  let temp = fromCurr.value;
+  fromCurr.value = toCurr.value;
+  toCurr.value = temp;
+
+  updateFlag(fromCurr);
+  updateFlag(toCurr);
+  updateExchangeRate();
+});
+
+
+const amountInput = document.querySelector(".amount input");
+
+amountInput.addEventListener("input", () => {
+  updateExchangeRate();
+});
+
+// msg.innerText = "Fetching latest exchange rate...";
+const modeBtn = document.getElementById("mode");
+
+modeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
